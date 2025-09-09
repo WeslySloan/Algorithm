fb4967e82c8ea484ecb1d9988797dab980c44147
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2025-09-09 09:34:28 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int n)
+{
+    int answer = 0;
+
+    while (n >= a)
+    {
+        int newcola = (n / a) * b;
+        answer = answer + newcola;
+        n = (n % a) + newcola;
+    }
+    
+    return answer;
+}
